501c6b617a0041927aa11b65061c2654f4c1a8cb
Shaun Luttin <admin@shaunluttin.com>
2017-07-04 11:12:32 -0700

    Add points of LoD slide

--- LaTeX/src/simple/only-talk-to-your-friends.ts
+++ LaTeX/src/simple/only-talk-to-your-friends.ts
@@ -6,14 +6,15 @@ class Farmer {
 
     // A method of an object may only call methods of: 
     public DigHole(place: Place) {
-
         const shovel = new Shovel();
-
         while (this.energyLevel > 0) {
+
             // 1. The object itself.
             this.decreaseEnergyLevel();
+
             // 2. Any argument of the method.
             const target = place.getHighestPlaceWithin();
+
             // 3. Any object created within the method.
             shovel.dig(target);
         }
