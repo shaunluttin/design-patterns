8248b8e1d5c9005b2acb6f95dfdaa63fab41e643
Shaun Luttin <admin@shaunluttin.com>
2017-07-04 12:59:32 -0700

    Add an SRP example

---
+++ LaTeX/src/simple/one-reason-to-change.ts
@@ -0,0 +1,25 @@
+class RaisedBed {
+
+    public addCompost() { }
+
+    public addMulch() { }
+
+    public addWater() { }
+
+    public addSeeds() { }
+
+    public harvestProduce() { }
+
+    public pullWeeds() { }
+}
+
+/* 
+ * There are several responsibilities here: 
+ * 
+ * 1. preparing the raised bed before planting
+ * 2. maintaining it after planting
+ * 3. harvesting
+ * 
+ * It's likely that the watering system will change independently of our harvesting system.
+ * 
+ */
