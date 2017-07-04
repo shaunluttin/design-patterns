917c50f8ea05eb0fd94b8198c689203c01d68b2f
Shaun Luttin <admin@shaunluttin.com>
2017-07-04 09:38:58 -0700

    Add an example of encapsulate what varies

--- LaTeX/src/simple/encapsulate-what-varies.ts
+++ LaTeX/src/simple/encapsulate-what-varies.ts
@@ -1,2 +1,26 @@
-// TODO: Add a few technique for encapsulating what varies.
-// Heuristic: "What do you want to be able to change without redesign?"
+class Product {
+    public price: number;
+}
+
+// We have encapsulated the calculation of tax.
+class TaxCalculator {
+    public calculateTax(product: Product): number {
+        throw new Error("TODO: Calculate the tax on the product.");
+    }
+}
+
+class FarmStand {
+
+    private cart: Array<Product>;
+
+    public DisplayGrandTotal(): number {
+        const taxCalculator = new TaxCalculator();
+
+        return this.cart.reduce((sum, product) => {
+            // Question: What else might we want to change without redesign?
+            const tax = taxCalculator.calculateTax(product);
+            const productGrandTotal = tax + product.price;
+            return sum + productGrandTotal;
+        }, 0);
+    }
+}
