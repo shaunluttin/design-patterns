095e498146f390a3774d2c640fb999fb10b3ff6d
Shaun Luttin <admin@shaunluttin.com>
2017-08-18 20:45:30 -0700

    Add an EnchantedMazeGame

---
+++ CSharp/Creational/FactoryMethod/EnchantedMazeGame.cs
@@ -0,0 +1,22 @@
+using CreationalPatterns.Models;
+
+namespace CreationalPatterns.FactoryMethod
+{
+    public class EnchantedMazeGame : MazeGame
+    {
+        protected override Room MakeRoom(int n)
+        {
+            return new EnchantedRoom(n, CastSpell());
+        }
+
+        protected override Door MakeDoor(Room room1, Room room2)
+        {
+            return new DoorNeedingSpell(room1, room2);
+        }
+
+        private Spell CastSpell()
+        {
+            return new Spell();
+        }
+    }
+}
