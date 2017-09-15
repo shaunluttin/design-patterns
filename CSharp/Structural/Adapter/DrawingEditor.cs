633c047b20c07c937f17f31d2d04a5186a0bf2ad
Shaun Luttin <admin@shaunluttin.com>
2017-09-14 19:17:37 -0700

    Make the adapter demo runable

--- CSharp/Structural/Adapter/DrawingEditor.cs
+++ CSharp/Structural/Adapter/DrawingEditor.cs
@@ -18,19 +18,9 @@ namespace Structural.Adapter
             }
         }
 
-        public void AddTextView()
+        public void AddShape(IShape shape)
         {
-            // class adapter
-            var textShape01 = new ClassAdapter.TextShape();
-
-            // object adapter: more flexible at the cost of more code
-            var textView = new TextView();
-            var textShape02 = new ObjectAdapter.TextShape(textView);
-
-            // Clients that expect an `IShape`
-            // can now work with a `TextView`.
-            _shapes.Add(textShape01);
-            _shapes.Add(textShape02);
+            _shapes.Add(shape);
         }
     }
 }
--- CSharp/Structural/Program.cs
+++ CSharp/Structural/Program.cs
@@ -8,5 +8,21 @@ namespace Structural
         {
             var program = new Program();
         }
+
+        public void Adapter() 
+        {
+            var editor = new Adapter.DrawingEditor();
+
+            // class adapter
+            var textShape01 = new Adapter.ClassAdapter.TextShape();
+
+            // object adapter: more flexible at the cost of more code
+            var textView = new Adapter.TextView();
+            var textShape02 = new Adapter.ObjectAdapter.TextShape(textView);
+
+            // Clients that expect an `IShape`
+            // can now work with a `TextView`.
+            editor.AddShape(textShape01);
+        }
     }
 }
