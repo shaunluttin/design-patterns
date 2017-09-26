470f8ddd9aa2f6b8a93e499158e8d5cdcff949f5
Shaun Luttin <admin@shaunluttin.com>
2017-09-25 18:17:33 -0700

    Add an abstract Observer and Subject class

---
+++ CSharp/Behavioral/Observer/Observer.cs
@@ -0,0 +1,7 @@
+namespace Behavioral.Observer
+{
+    public abstract class Observer
+    {
+        public abstract void Update(Subject theChangedSubject);
+    }
+}
---
+++ CSharp/Behavioral/Observer/Subject.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+
+namespace Behavioral.Observer
+{
+    public abstract class Subject
+    {
+        private List<Observer> _observers = new List<Observer>();
+
+        public virtual void Attach(Observer o)
+        {
+            _observers.Add(o);
+        }
+
+        public virtual void Detach(Observer o)
+        {
+            _observers.Remove(o);
+        }
+
+        public virtual void Notify()
+        {
+            foreach (var observer in _observers)
+            {
+                observer.Update(this);    
+            }
+        }
+    }
+}
